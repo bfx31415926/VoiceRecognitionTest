e2d9cab4f0032ad3710dd6acc87c3eb9e938cae2
smirnov_aa <bfx-683@yandex.ru>
2022-01-16 22:56:20 +0300

    chore: Немного поизучал

--- app/src/main/java/com/example/voicerecognitiontest/VoiceRecognitionTest.java
+++ app/src/main/java/com/example/voicerecognitiontest/VoiceRecognitionTest.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 import java.util.Locale;
 
 import android.util.Log;
+import android.widget.Toast;
 
 public class VoiceRecognitionTest extends Activity implements OnClickListener
 {
@@ -22,7 +23,7 @@ public class VoiceRecognitionTest extends Activity implements OnClickListener
     private TextView mText;
     private TextView words;
     private SpeechRecognizer sr;
-    private static final String TAG = "MyStt3Activity";
+    private static final String TAG = "MyTag";
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -42,27 +43,42 @@ public class VoiceRecognitionTest extends Activity implements OnClickListener
         public void onReadyForSpeech(Bundle params)
         {
             Log.d(TAG, "onReadyForSpeech");
+//            Toast.makeText(VoiceRecognitionTest.this, "onReadyForSpeech", Toast.LENGTH_SHORT).show();
         }
         public void onBeginningOfSpeech()
         {
             Log.d(TAG, "onBeginningOfSpeech");
+//            Toast.makeText(VoiceRecognitionTest.this, "onBeginningOfSpeech", Toast.LENGTH_SHORT).show();
         }
         public void onRmsChanged(float rmsdB)
         {
             Log.d(TAG, "onRmsChanged");
+//            Toast.makeText(VoiceRecognitionTest.this, "onRmsChanged", Toast.LENGTH_SHORT).show();
         }
         public void onBufferReceived(byte[] buffer)
         {
             Log.d(TAG, "onBufferReceived");
+//            Toast.makeText(VoiceRecognitionTest.this, "onBufferReceived", Toast.LENGTH_SHORT).show();
         }
         public void onEndOfSpeech()
         {
             Log.d(TAG, "onEndofSpeech");
+//            Toast.makeText(VoiceRecognitionTest.this, "onEndofSpeech", Toast.LENGTH_SHORT).show();
         }
         public void onError(int error)
         {
             Log.d(TAG,  "error " +  error);
             mText.setText("error " + error);
+            String errS;
+            if(error == 2)
+                errS = "Отсутствует Интернет-соединение. Включите его!";
+            else if(error == 7)
+                errS = "Речь не распознана";
+            else
+                errS = "Проблемы речевого ввода";
+
+            Toast.makeText(VoiceRecognitionTest.this, errS, Toast.LENGTH_SHORT).show();
+
         }
         public void onResults(Bundle results)
         {
@@ -80,18 +96,21 @@ public class VoiceRecognitionTest extends Activity implements OnClickListener
         public void onPartialResults(Bundle partialResults)
         {
             Log.d(TAG, "onPartialResults");
+//            Toast.makeText(VoiceRecognitionTest.this, "onPartialResults", Toast.LENGTH_SHORT).show();
         }
         public void onEvent(int eventType, Bundle params)
         {
             Log.d(TAG, "onEvent " + eventType);
+//            Toast.makeText(VoiceRecognitionTest.this, "onEvent " + eventType, Toast.LENGTH_SHORT).show();
         }
     }
     public void onClick(View v) {
         if (v.getId() == R.id.btn_speak)
         {
             Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-            intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,"voice.recognition.test");
+//            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
+            intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,"com.example.voicerecognitiontest");
 
             String languagePref = "ru_RU";
             intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, languagePref);
